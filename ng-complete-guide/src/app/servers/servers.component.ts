41138fb9f309212393ba84f0dde2f32736f80d12
gsuvodip <gsuvodip.webdev@gmail.com>
2024-01-20 22:50:28 +0530

    data binding - property binding

--- ng-complete-guide/src/app/servers/servers.component.ts
+++ ng-complete-guide/src/app/servers/servers.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: 'app-servers',  //  element selector
   // selector: '[app-servers]',  //  attribute selector
   selector: '.app-servers',  //  class selector
-  // templateUrl: './servers.component.html',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>
-  `,
+  templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `,
   styleUrl: './servers.component.css'
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
+
 
 }
