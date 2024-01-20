b58b9ba60c9ecf8dc1d89aeb4096130928a9d975
gsuvodip <gsuvodip.webdev@gmail.com>
2024-01-20 15:39:00 +0530

    inline styles

--- ng-complete-guide/src/app/app.component.ts
+++ ng-complete-guide/src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css'
+  styles: [
+    `
+    h1 {
+      color: lightblue;
+    }
+    `
+  ]
 })
 export class AppComponent {
 }
